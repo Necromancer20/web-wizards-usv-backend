3cafda03e9fdfd21ce5ca727d6fcc9159a97b295
Xhadrines <sandru99alex@gmail.com>
2024-11-21 16:52:43 +0200

    0.1.1
    Update rquirements.txt + test.sql

--- interogareSQL/test.sql
+++ interogareSQL/test.sql
@@ -35,10 +35,10 @@ VALUES
 -- 4. Inserare Utilizatori (Profesori, Studenti)
 INSERT INTO utilizator (id, first_name, last_name, email, password, rol)
 VALUES
-  (NEWID(), 'Ion', 'Popescu', 'ion.popescu@univ.com', 'parola123', 'professor'),
-  (NEWID(), 'Maria', 'Ionescu', 'maria.ionescu@univ.com', 'parola123', 'student'),
-  (NEWID(), 'Alex', 'Vasile', 'alex.vasile@univ.com', 'parola123', 'student'),
-  (NEWID(), 'Ana', 'Georgescu', 'ana.georgescu@univ.com', 'parola123', 'professor');
+  (NEWID(), 'Ion', 'Popescu', 'ion.popescu@univ.com', 'parola123', 'PROFESSOR'),
+  (NEWID(), 'Maria', 'Ionescu', 'maria.ionescu@univ.com', 'parola123', 'STUDENT'),
+  (NEWID(), 'Alex', 'Vasile', 'alex.vasile@univ.com', 'parola123', 'STUDENT'),
+  (NEWID(), 'Ana', 'Georgescu', 'ana.georgescu@univ.com', 'parola123', 'PROFESSOR');
 
 -- 5. Inserare Materii
 INSERT INTO materii (id, id_grupa, id_profesor, semestrul, nume, nume_abreviat, numar_credite, durata_examen_minute)
@@ -51,10 +51,10 @@ VALUES
 -- 6. Inserare Programări Examen
 INSERT INTO programari_examen (id, id_materie, id_profesor, id_student_creator, id_grupa, data_examen, locatie, tip_examen, observatii, status)
 VALUES
-(NEWID(), (SELECT id FROM materii WHERE nume = 'Matematica Discreta'), (SELECT id FROM utilizator WHERE email = 'ion.popescu@univ.com'), (SELECT id FROM utilizator WHERE email = 'alex.vasile@univ.com'), (SELECT id FROM grupe WHERE numar_grupa = 101), '2024-12-15 10:00:00', 'Sala 101', 'examen', 'Examen de nivel mediu', 'in_asteptare'),
-(NEWID(), (SELECT id FROM materii WHERE nume = 'Literatura Romana'), (SELECT id FROM utilizator WHERE email = 'ana.georgescu@univ.com'), (SELECT id FROM utilizator WHERE email = 'maria.ionescu@univ.com'), (SELECT id FROM grupe WHERE numar_grupa = 102), '2024-12-16 14:00:00', 'Sala 102', 'coloc', 'Colocviu final', 'in_asteptare'),
-(NEWID(), (SELECT id FROM materii WHERE nume = 'Programare Orientata pe Obiect'), (SELECT id FROM utilizator WHERE email = 'ion.popescu@univ.com'), (SELECT id FROM utilizator WHERE email = 'alex.vasile@univ.com'), (SELECT id FROM grupe WHERE numar_grupa = 103), '2024-12-17 09:00:00', 'Sala 103', 'examen', 'Examen final pe obiecte', 'in_asteptare'),
-(NEWID(), (SELECT id FROM materii WHERE nume = 'Contabilitate Financiara'), (SELECT id FROM utilizator WHERE email = 'ana.georgescu@univ.com'), (SELECT id FROM utilizator WHERE email = 'maria.ionescu@univ.com'), (SELECT id FROM grupe WHERE numar_grupa = 104), '2024-12-18 12:00:00', 'Sala 104', 'coloc', 'Colocviu de evaluare', 'in_asteptare');
+(NEWID(), (SELECT id FROM materii WHERE nume = 'Matematica Discreta'), (SELECT id FROM utilizator WHERE email = 'ion.popescu@univ.com'), (SELECT id FROM utilizator WHERE email = 'alex.vasile@univ.com'), (SELECT id FROM grupe WHERE numar_grupa = 101), '2024-12-15 10:00:00', 'Sala 101', 'examen', 'Examen de nivel mediu', 'IN_ASTEPTARE'),
+(NEWID(), (SELECT id FROM materii WHERE nume = 'Literatura Romana'), (SELECT id FROM utilizator WHERE email = 'ana.georgescu@univ.com'), (SELECT id FROM utilizator WHERE email = 'maria.ionescu@univ.com'), (SELECT id FROM grupe WHERE numar_grupa = 102), '2024-12-16 14:00:00', 'Sala 102', 'coloc', 'Colocviu final', 'IN_ASTEPTARE'),
+(NEWID(), (SELECT id FROM materii WHERE nume = 'Programare Orientata pe Obiect'), (SELECT id FROM utilizator WHERE email = 'ion.popescu@univ.com'), (SELECT id FROM utilizator WHERE email = 'alex.vasile@univ.com'), (SELECT id FROM grupe WHERE numar_grupa = 103), '2024-12-17 09:00:00', 'Sala 103', 'examen', 'Examen final pe obiecte', 'IN_ASTEPTARE'),
+(NEWID(), (SELECT id FROM materii WHERE nume = 'Contabilitate Financiara'), (SELECT id FROM utilizator WHERE email = 'ana.georgescu@univ.com'), (SELECT id FROM utilizator WHERE email = 'maria.ionescu@univ.com'), (SELECT id FROM grupe WHERE numar_grupa = 104), '2024-12-18 12:00:00', 'Sala 104', 'coloc', 'Colocviu de evaluare', 'IN_ASTEPTARE');
 
 -- 7. Inserare Grupe Utilizator
 INSERT INTO grupe_utilizator (id_utilizator, id_grupa, este_sef_semigrupa)
